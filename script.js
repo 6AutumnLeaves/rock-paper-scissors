138bbe922543fb3cbdfc2ab6e4adcb447c2d3a9d
Justus Bartmangit config --global user.email justus.bartman@gmail.com <justus.bartman@gmail.com>
2022-06-12 10:00:35 -0400

    Finished computerPlay function

--- script.js
+++ script.js
@@ -4,4 +4,25 @@
 //Pick number 1-3
 //1 = rock; 2 = paper; 3 = scissors
 //return value
-let computerPlay = () => console.log(Math.floor(Math.random() * 3));
+
+
+// let computerPlay = () => console.log(Math.floor(Math.random() * 3));
+function computerPlay(random) {
+    let compNumber = (Math.floor(Math.random() * 3));
+    let compChoice = undefined;
+    if (compNumber === 0) {
+        return compChoice = "Rock";
+    } else if (compNumber === 1) {
+      return compChoice = "Paper";
+    } else {
+        return compChoice = "Scissors";
+    }
+}
+
+//this works, but not as I expected
+//values are from 0-2 not 1-3
+// this will still work though
+//so far compPlay return 0-2
+//I need if statements for this to return "Rock", "Paper", or "Scissors"
+
+
